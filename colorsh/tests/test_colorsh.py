b451904d5af0ebe85aad17709c9f2c690c1106bc
radiand <radiand@protonmail.com>
2017-05-25 22:48:40 +0200

    new UTs for Colorsh with Style class

--- colorsh/tests/test_colorsh.py
+++ colorsh/tests/test_colorsh.py
@@ -1,5 +1,5 @@
 import unittest
-from colorsh import Colorsh, Color, Encoding, ANSI_ESCAPE_SEQUENCE
+from colorsh import Colorsh, Color, Style, Encoding, ANSI_ESCAPE_SEQUENCE
 
 class TestColorsh(unittest.TestCase):
     def test_not_decorate_if_no_encoding(self):
@@ -26,6 +26,23 @@ class TestColorsh(unittest.TestCase):
             ANSI_ESCAPE_SEQUENCE + "0;38;5;9;48;5;12m" + "example" + ANSI_ESCAPE_SEQUENCE + "0m",
             Colorsh.decorate("example", enc=Encoding.ansi, fg=Color("red"), bg=Color("blue")))
 
+    def test_decorate_ansi_style(self):
+        self.assertEqual(
+            ANSI_ESCAPE_SEQUENCE + "1m" + "example" + ANSI_ESCAPE_SEQUENCE + "0m",
+            Colorsh.decorate("example", enc=Encoding.ansi, style=Style(["bold"])))
+
+    def test_decorate_ansi_style_takes_only_first_style_from_list(self):
+        self.assertEqual(
+            ANSI_ESCAPE_SEQUENCE + "1m" + "example" + ANSI_ESCAPE_SEQUENCE + "0m",
+            Colorsh.decorate("example", enc=Encoding.ansi, style=Style(["bold", "faint", 3])))
+
+    def test_decorate_ansi_color_and_style(self):
+        self.assertEqual(
+            ANSI_ESCAPE_SEQUENCE + "1;38;5;9;48;5;12m" + "example" + ANSI_ESCAPE_SEQUENCE + "0m",
+            Colorsh.decorate(
+                "example", enc=Encoding.ansi, fg=Color("red"), bg=Color("blue"),
+                style=Style(["bold"])))
+
     def test_decorate_tmux_color_fg(self):
         self.assertEqual(
             "#[fg=colour9]example",
@@ -40,3 +57,20 @@ class TestColorsh(unittest.TestCase):
         self.assertEqual(
             "#[fg=colour9,bg=colour12]example",
             Colorsh.decorate("example", enc=Encoding.tmux, fg=Color("red"), bg=Color("blue")))
+
+    def test_decorate_tmux_style(self):
+        self.assertEqual(
+            "#[fg=bold]example",
+            Colorsh.decorate("example", enc=Encoding.tmux, style=Style(["bold"])))
+
+        self.assertEqual(
+            "#[fg=bold,italics]example",
+            Colorsh.decorate("example", enc=Encoding.tmux, style=Style(["bold", "italics"])))
+
+    def test_decorate_tmux_color_and_style(self):
+        self.assertEqual(
+            "#[fg=colour9,bold,italics,bg=colour12]example",
+            Colorsh.decorate(
+                "example", enc=Encoding.tmux, fg=Color("red"), bg=Color("blue"),
+                style=Style(["bold", "italics"])))
+
